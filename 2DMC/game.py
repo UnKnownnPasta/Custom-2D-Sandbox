b60140a3dde6b05534522ddb935811b04dde49ab
UnKnownn <pandaepic01@gmail.com>
2023-03-17 09:26:06 +0530

    addition of window creation

--- 2DMC/game.py
+++ 2DMC/game.py
@@ -1,5 +1,8 @@
-import csv
-import subprocess
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont, QIcon, QCursor, QPixmap
+from PyQt5.QtCore import Qt
+import sys, csv, subprocess
 
 p = subprocess.Popen(['python', '2DMC/mapcreate.py'], stderr=subprocess.PIPE)
 
@@ -15,7 +18,38 @@ for row in reader:
 
 TerrainOne, TerrainTwo, TerrainThree, CaveMap, hotbar, inventory = [list(row) for row in load]
 
-x = 1
+def main():
+    global app
+    app = QApplication([])
+    window = QWidget()
+    layout = QHBoxLayout()
 
-while True:
-    pass
+    logo = QLabel(window)
+    logo.setPixmap(QPixmap('./2DMC/logo.jpg'))
+    logo.setFixedSize(20, 20)
+    
+    title = QLabel(window)
+    title.setText('MC 2D Sandbox')
+    title.move(35, 5)
+
+    window.setCursor(QCursor(QPixmap('./2DMC/cursor.cur'), 8, 3))
+    window.setWindowFlags(Qt.FramelessWindowHint)
+    window.setFixedSize(900, 600)
+
+    xButton = QPushButton(QIcon('./2DMC/xbutton.png'), '')
+    xButton.setStyleSheet('QPushButton { border: none; }')
+    xButton.setFixedSize(23, 23)
+    xButton.clicked.connect(onXClick)
+
+    layout.addWidget(logo, 0,  Qt.AlignTop | Qt.AlignLeft)
+    layout.addWidget(xButton, 0, Qt.AlignTop | Qt.AlignRight)
+
+    window.setLayout(layout)
+
+    window.show()
+    app.exec_()
+
+def onXClick(): app.exit()
+
+if __name__ == '__main__':
+    main()
